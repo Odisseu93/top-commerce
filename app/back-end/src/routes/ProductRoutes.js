2ee538558249a00c321222f0e09f3cf9cea13791
@Odisseu93 - Ulisses Silvério <ulissessuporteti01@gmail.com>
2023-09-02 20:18:42 -0300

    feat: add update product route

--- app/back-end/src/routes/ProductRoutes.js
+++ app/back-end/src/routes/ProductRoutes.js
@@ -6,5 +6,6 @@ const ProductController = require('../controllers/ProductController')
 
 router.get('/products/all', ProductController.getAll)
 router.post('/products/', ProductController.create)
+router.put('/products/', ProductController.update)
 
 module.exports = router
