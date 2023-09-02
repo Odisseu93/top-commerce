f9480ddd614d8456b4307f9c95af62e2b5b06d18
@Odisseu93 - Ulisses Silvério <ulissessuporteti01@gmail.com>
2023-09-02 19:48:30 -0300

    feat: add create product route

--- app/back-end/src/routes/ProductRoutes.js
+++ app/back-end/src/routes/ProductRoutes.js
@@ -5,5 +5,6 @@ const router = express.Router()
 const ProductController = require('../controllers/ProductController')
 
 router.get('/products/all', ProductController.getAll)
+router.post('/products/', ProductController.create)
 
 module.exports = router
