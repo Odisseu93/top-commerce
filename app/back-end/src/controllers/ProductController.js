52d0c5d25290b6b5b9397a76c0bd3f05797b6e35
@Odisseu93 - Ulisses Silvério <ulissessuporteti01@gmail.com>
2023-09-02 19:26:52 -0300

    feat: add ProductController.create

--- app/back-end/src/controllers/ProductController.js
+++ app/back-end/src/controllers/ProductController.js
@@ -1,9 +1,31 @@
 const Product = require('../models/Product')
-
+const { randomUUID } = require('crypto')
 class ProductController {
+
   static async getAll(req, res) {
     await Product.findAll()
       .then(products => res.status(200).json({ products: products }))
+      .catch(err => { throw new Error(err) })
+  }
+
+
+  static async create(req, res) {
+    const { name, description, category } = req.body
+    const uuid = randomUUID()
+
+    const newProduct = {
+      id: uuid,
+      name: name,
+      description: description,
+      category: category,
+      active: false
+    }
+
+    await Product.create(newProduct)
+      .then(product => res.status(201).json({
+        message: 'Product created seccessfully!',
+        registeredProduct: product
+      }))
       .catch(err => { throw new Error(err) })
   }
 }
