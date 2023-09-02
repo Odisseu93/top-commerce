39252f9b50dedba42c744318324861980c94e5bf
@Odisseu93 - Ulisses Silvério <ulissessuporteti01@gmail.com>
2023-09-02 20:27:38 -0300

    feat: add delete product route

--- app/back-end/src/routes/ProductRoutes.js
+++ app/back-end/src/routes/ProductRoutes.js
@@ -7,5 +7,6 @@ const ProductController = require('../controllers/ProductController')
 router.get('/products/all', ProductController.getAll)
 router.post('/products/', ProductController.create)
 router.put('/products/', ProductController.update)
+router.delete('/products/:id', ProductController.delete)
 
 module.exports = router
