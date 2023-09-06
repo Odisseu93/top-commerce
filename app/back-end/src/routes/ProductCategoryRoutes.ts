78098c42f907e72de6bf84c9b79d855576268262
@Odisseu93 - Ulisses Silvério <ulissessuporteti01@gmail.com>
2023-09-06 19:30:43 -0300

    59bdfe2: feat: add product category inative route

--- app/back-end/src/routes/ProductCategoryRoutes.ts
+++ app/back-end/src/routes/ProductCategoryRoutes.ts
@@ -8,5 +8,6 @@ router.get('/', ProductCategoryController.getAll);
 router.get('/:id', ProductCategoryController.getById);
 router.post('/', ProductCategoryController.create);
 router.put('/:id', ProductCategoryController.update);
+router.delete('/:id', ProductCategoryController.inative);
 
 export default router;
